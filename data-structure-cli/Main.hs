adf58a7687d756451202b2b1bf3dd6f641a04e01
vosmann <vosmann@gmail.com>
2017-06-09 14:16:56 +0200

    Support add and remove on list in a state monad.

--- data-structure-cli/Main.hs
+++ data-structure-cli/Main.hs
@@ -2,40 +2,21 @@ module Main where
 
 import State.StateMonad
 
-{-
-add :: String -> ST [String]
-add x (S st) = S(\xs -> ([x], x:xs))
+main :: IO ()
+main = run []
 
-remove :: String -> ST [String]
-remove x = S(\xs -> let (removed,left) = partition (==x) xs in (removed,left))
--}
+run :: [String] ->  IO ()
+run s = do putStrLn "Command"
+              command <- getLine
+              putStrLn "Element: "
+              element <- getLine
+              let s' = mutate command element s
+              putStrLn "New state: "
+              putStrLn (show s')
+              sranje s'
 
-list :: IO ()
-list = do putStr "Enter command ('add' or 'remove'): "
-          command <- getLine
-          case command of
-                    "add"    -> do putStr "(Add) Enter element: "
-                                   element <- getLine
-                                   putStrLn "New element: "
-                                   putStrLn element
-                                   putStrLn "New state: "
-                                   putStrLn "fake state after addition"
-                                   list
-                    "remove" -> do putStr "(Remove) Enter element: "
-                                   element <- getLine
-                                   putStrLn "Removed element: "
-                                   putStrLn element
-                                   putStrLn "New state: "
-                                   putStrLn "fake state after removal"
-                                   list
-                    _        -> do putStrLn "Unsupported command."
-                                   list
-
-
-
-partition :: (a -> Bool) -> [a] -> ([a], [a])
-partition p xs = part p ([],[]) xs
-                 where part p (ts,fs)     [] = (ts,fs)
-                       part p (ts,fs) (x:xs) = if p x then part p (x:ts,fs) xs else part p (ts,x:fs) xs
-
-
+mutate cmd elem s = execState statemonad s 
+                      where statemonad = case cmd of
+                                             "add"    -> modify (elem:)
+                                             "remove" -> modify (filter (/=elem))
+                                             _        -> put s
