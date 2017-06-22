4b24bc8851bb83ae6884d3aea7c1d05f72171480
vosmann <vosmann@gmail.com>
2017-06-22 18:26:04 +0200

    Extract commands and elements form query parameters.

--- data-structure-http/Main.hs
+++ data-structure-http/Main.hs
@@ -1,71 +1,45 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-{-# LANGUAGE OverloadedStrings #-}
 import Network.Wai
 import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
 
 import Data.IORef (IORef, newIORef, atomicModifyIORef)
-import Data.ByteString.Lazy.Char8 (pack)
+--import Data.ByteString.Lazy.Char8 (pack, unpack)
+import Data.ByteString.Lazy.Char8
+import Control.Monad (join)
 import Control.Monad.Trans (liftIO, lift)
-
-{-
-import Control.Monad.Trans.Resource (ResourceT)
-import Data.ByteString.Lazy as B (concat, ByteString, append)
-import Data.ByteString.Lazy.UTF8 (fromString)
-
-application :: (Num a, Show a) => IORef a -> Request -> ResourceT IO Response
-application counter request = do  -- (3)
-    count <- lift $ incCount counter  -- (5)
-    liftIO $ printCount count -- (6)
-    let responseText = 
-    return $ make
-
-makeResponse = responseLBS status200 [("Content-type", "text/html")] $ responseText -- (7), (8)
-(makeResponseText count)
-
-makeResponseText :: (Show a) => a -> B.ByteString
-makeResponseText s = "<h1>Hello World " `append` (toByteString s) `append` "</h1>\n"
-
-toByteString :: (Show a) => a -> B.ByteString
-toByteString s = fromString $ show s
-
-
-main = do 
-    counter <- newIORef 0 -- (1)
-    run 3000 $ application counter -- (2)
--}
-
--- app :: IORef [a] -> (Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived)
-app :: IORef [String] -> Application
-app list request respond = do let params = queryString request
-                              let respString = pack $ show params
-                              putStrLn $ "Query params: " ++ (show params)
-                              --l <- lift $ inc list
-                              l <- inc list
-                              --liftIO $ print' l
-                              putStrLn $ show l
-                              respond $ responseLBS status200 [("Content-Type", "text/plain")] respString
 
 main :: IO ()
 main = do putStrLn $ "Listening on port " ++ show 8080
           list <- newIORef []
           run 8080 (app list)
 
-inc :: IORef [String] -> IO [String]
-inc list = atomicModifyIORef list (\s -> ("lol":s, s))
+-- app :: IORef [a] -> (Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived)
+app :: IORef [String] -> Application
+app list request respond = do putStrLn $ logged request
+                              let cmd = param request "command"
+                                  elem = param request "element"
+                              l <- inc cmd elem list 
+                              putStrLn $ show l
+                              respond $ responseLBS status200 [("Content-Type", "text/plain")] (pack $ show l)
 
---print' :: (Show a) => a -> IO ()
---print' x = do putStrLn $ "New state: " ++ show x
+-- unpack instead of show?
+logged req = concat $ map show [requestMethod req,
+                                rawPathInfo req,
+                                rawQueryString req]
 
---mutate :: Eq a => String -> a -> [a] -> [a]
---mutate cmd elem s = case cmd of
---                        "add" -> (elem:s)
---                        "rem" -> filter (/=elem) s
---                        _     -> s
---mutate :: Eq a => Text -> a -> [a] -> ([a], Int)
---mutate cmd elem s = let result = case cmd of
---                                     "add" -> (elem:s)
---                                     "rem" -> filter (/=elem) s
---                                     _     -> s
---                    in (result, result)
+param req name = fmap unpack (join (lookup (pack name) (getParams req)))
+
+inc :: Maybe String -> Maybe String -> IORef [String] -> IO [String]
+inc cmd elem = modify f
+               where f = chooseMod cmd elem 
+                     modify f list = atomicModifyIORef list (\s -> (f s, f s))
+
+chooseMod :: Maybe String -> Maybe String -> ([String] -> [String])
+chooseMod (Just "add") (Just elem) = (\xs -> elem:xs)
+chooseMod (Just "rem") (Just elem) = (\xs -> filter (/=elem) xs)
+chooseMod _            _           = id
+
+getParams req = map toString (queryString req)
+                where toString (x, y) = (unpack x, fmap unpack y)
