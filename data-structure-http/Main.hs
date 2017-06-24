afddb9180880d5791cc13c5a973862bebfe279bf
vosmann <vosmann@gmail.com>
2017-06-24 12:11:33 +0200

    Switched to ByteString.

--- data-structure-http/Main.hs
+++ data-structure-http/Main.hs
@@ -4,11 +4,13 @@ import Network.Wai
 import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
 
-import Prelude hiding (map, concat, putStrLn, filter)
+import Prelude hiding (map, concat, putStrLn)
 
 import Data.IORef (IORef, newIORef, atomicModifyIORef)
 --import Data.ByteString.Lazy.Char8 (pack, unpack)
-import Data.ByteString.Lazy.Char8 
+-- Wai uses strict ByteStrings.
+import Data.ByteString
+import Data.ByteString.Lazy (fromChunks)
 import Control.Monad (join)
 import Control.Monad.Trans (liftIO, lift)
 
@@ -16,23 +18,26 @@ default (ByteString)
 
 main :: IO ()
 main = do putStrLn $ "Listening on port 8080"
-          list <- newIORef []
-          run 8080 (app list)
+          listRef <- newIORef []
+          run 8080 (app listRef)
 
 -- app :: IORef [a] -> (Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived)
 app :: IORef [ByteString] -> Application
-app list request respond = do putStrLn $ logged request
-                              let cmd = param request "command"
-                                  elem = param request "element"
-                              l <- inc cmd elem list 
-                              putStrLn l
-                              respond $ responseLBS status200 [("Content-Type", "text/plain")] (show l)
+app listRef request respond = do putStrLn $ logged request
+                                 let cmd = param request "command"
+                                     elem = param request "element"
+                                 list <- inc cmd elem listRef
+                                 putStrLn $ concat list -- show for bytestrings?
+                                 respond $ responseLBS status200
+                                                       [("Content-Type", "text/plain")]
+                                                       (fromChunks list)
 
--- unpack instead of show?
+-- show for bytestrings?
 logged req = concat [requestMethod req,
                      rawPathInfo req,
                      rawQueryString req]
 
+-- queryString returns [(ByteString, Maybe ByteString)], i.e. strict ByteStrings.
 param req name = join (lookup name (queryString req))
 
 inc :: Maybe ByteString -> Maybe ByteString -> IORef [ByteString] -> IO [ByteString]
@@ -42,8 +47,5 @@ inc cmd elem = modify f
 
 chooseMod :: Maybe ByteString -> Maybe ByteString -> ([ByteString] -> [ByteString])
 chooseMod (Just "add") (Just elem) = (\xs -> elem:xs)
-chooseMod (Just "rem") (Just elem) = (\xs -> filter (/=elem) xs)
+chooseMod (Just "rem") (Just elem) = (\xs -> Prelude.filter (/=elem) xs)
 chooseMod _            _           = id
-
---getParams req = map toString (queryString req)
---                where toString (x, y) = (unpack x, fmap unpack y)
